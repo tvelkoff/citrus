0a64a34d204d3785ec49f4922a4d3a32f098236e
tjvelkof <theodore.j.velkoff@associates.ice.dhs.gov>
2018-12-10 16:12:27 -0500

    Add methods to facilitate testing
    New methods take StringResult as an argument so it can be mocked. Public
    methods call these and pass null, keeping the same public signature.

--- modules/citrus-java-dsl/src/main/java/com/consol/citrus/dsl/builder/ReceiveMessageBuilder.java
+++ modules/citrus-java-dsl/src/main/java/com/consol/citrus/dsl/builder/ReceiveMessageBuilder.java
@@ -338,10 +338,23 @@ public class ReceiveMessageBuilder<A extends ReceiveMessageAction, T extends Rec
      * @return
      */
     public T headerFragment(Object model) {
+    	return doHeaderFragment(model, null);
+//        Assert.notNull(applicationContext, "Citrus application context is not initialized!");
+//
+//        if (!CollectionUtils.isEmpty(applicationContext.getBeansOfType(Marshaller.class))) {
+//            return headerFragment(model, applicationContext.getBean(Marshaller.class));
+//        } else if (!CollectionUtils.isEmpty(applicationContext.getBeansOfType(ObjectMapper.class))) {
+//            return headerFragment(model, applicationContext.getBean(ObjectMapper.class));
+//        }
+//
+//        throw new CitrusRuntimeException("Unable to find default object mapper or marshaller in application context");
+    }
+    
+    protected T doHeaderFragment(Object model, StringResult stringResult) {
         Assert.notNull(applicationContext, "Citrus application context is not initialized!");
 
         if (!CollectionUtils.isEmpty(applicationContext.getBeansOfType(Marshaller.class))) {
-            return headerFragment(model, applicationContext.getBean(Marshaller.class));
+            return doHeaderFragment(model, applicationContext.getBean(Marshaller.class), stringResult);
         } else if (!CollectionUtils.isEmpty(applicationContext.getBeansOfType(ObjectMapper.class))) {
             return headerFragment(model, applicationContext.getBean(ObjectMapper.class));
         }
@@ -358,13 +371,32 @@ public class ReceiveMessageBuilder<A extends ReceiveMessageAction, T extends Rec
      * @return
      */
     public T headerFragment(Object model, String mapperName) {
+    	return doHeaderFragment(model, mapperName, null);
+//        Assert.notNull(applicationContext, "Citrus application context is not initialized!");
+//
+//        if (applicationContext.containsBean(mapperName)) {
+//            Object mapper = applicationContext.getBean(mapperName);
+//
+//            if (Marshaller.class.isAssignableFrom(mapper.getClass())) {
+//                return headerFragment(model, (Marshaller) mapper);
+//            } else if (ObjectMapper.class.isAssignableFrom(mapper.getClass())) {
+//                return headerFragment(model, (ObjectMapper) mapper);
+//            } else {
+//                throw new CitrusRuntimeException(String.format("Invalid bean type for mapper '%s' expected ObjectMapper or Marshaller but was '%s'", mapperName, mapper.getClass()));
+//            }
+//        }
+//
+//        throw new CitrusRuntimeException("Unable to find default object mapper or marshaller in application context");
+    }
+    
+    protected T doHeaderFragment(Object model, String mapperName, StringResult stringResult) {
         Assert.notNull(applicationContext, "Citrus application context is not initialized!");
 
         if (applicationContext.containsBean(mapperName)) {
             Object mapper = applicationContext.getBean(mapperName);
 
             if (Marshaller.class.isAssignableFrom(mapper.getClass())) {
-                return headerFragment(model, (Marshaller) mapper);
+                return doHeaderFragment(model, (Marshaller) mapper, stringResult);
             } else if (ObjectMapper.class.isAssignableFrom(mapper.getClass())) {
                 return headerFragment(model, (ObjectMapper) mapper);
             } else {
@@ -383,8 +415,26 @@ public class ReceiveMessageBuilder<A extends ReceiveMessageAction, T extends Rec
      * @return
      */
     public T headerFragment(Object model, Marshaller marshaller) {
-        StringResult result = new StringResult();
-
+    	return doHeaderFragment(model, marshaller, null);
+//        StringResult result = new StringResult();
+//
+//        try {
+//            marshaller.marshal(model, result);
+//        } catch (XmlMappingException e) {
+//            throw new CitrusRuntimeException("Failed to marshal object graph for message header data", e);
+//        } catch (IOException e) {
+//            throw new CitrusRuntimeException("Failed to marshal object graph for message header data", e);
+//        }
+//
+//        return header(result.toString());
+    }
+    
+    protected T doHeaderFragment(Object model, Marshaller marshaller, StringResult stringResult) {
+//        StringResult result = new StringResult();
+//        if (stringResult == null) {
+//        	result = new 
+//        }
+        StringResult result = stringResult == null ? new StringResult() : stringResult;
         try {
             marshaller.marshal(model, result);
         } catch (XmlMappingException e) {
@@ -896,6 +946,7 @@ public class ReceiveMessageBuilder<A extends ReceiveMessageAction, T extends Rec
      * Creates new script validation context if not done before and gets the script validation context.
      */
     private ScriptValidationContext getScriptValidationContext() {
+        Assert.notNull(this.messageType, "Message Type is not initialized!");
         if (scriptValidationContext == null) {
             scriptValidationContext = new ScriptValidationContext(messageType.toString());
 
